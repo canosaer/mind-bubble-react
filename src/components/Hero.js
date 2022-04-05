061e691a71883dd98888abf1d2dd43ad50126879
canosaer <canosaer@gmail.com>
2022-04-05 13:00:03 -0400

    more

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,10 +1,21 @@
 import React from 'react';
+import hero from '../img/hero.jpg'
 
 export default function Hero() {
 
 
     return(
-        <>
-        </>
+        <section className="hero">
+            <img className="hero__image" src={hero} alt="hero image" />
+            <h1 className="hero__heading">Mind Bubble</h1>
+            <h2 className="hero__subheading">Tutoring, Workshops, and Games</h2>
+
+            <div className="hero__menu">
+                <h2 className="hero__cta">Join our light-hearted learning community!</h2>
+                <a href="#" className="hero__button">Sign Up a Student</a>
+                <a href="#" className="hero__last-button">Sign Up to Volunteer</a>
+            </div>
+            
+        </section>
     )
 }
